ebe49ad271c0d201b811e01a8746cd054ca3b437
1203070045@qq.com <1203070045@qq.com>
2018-09-17 16:31:11 +0800

    支持64位的iTapTradeAPI_64.dll、TapQuoteAPI.dll

---
+++ ESITapTradeApiSimpleDemoPythonPyCharmC2018_X64/copy.bat
@@ -0,0 +1,5 @@
+xcopy ..\XTA_W64\Api\ESITap_v9.3.0.1_20160406 XTA_W64\Api\ESITap_v9.3.0.1_20160406 /I /E /Y
+copy ..\XTA_W64\Python\XFinApi_TradeApi.py XFinApi_TradeApi.py /Y
+copy ..\XTA_W64\Python\_XFinApi_TradeApi.cp36-win_amd64.pyd _XFinApi_TradeApi.cp36-win_amd64.pyd /Y
+
+pause
---
+++ ESITapTradeApiSimpleDemoVC2015_X64/copy.bat
@@ -0,0 +1,7 @@
+xcopy ..\XTA_W64\Api\ESITap_v9.3.0.1_20160406 x64\Release\XTA_W64\Api\ESITap_v9.3.0.1_20160406 /I /E /Y
+copy ..\XTA_W64\Cpp\XFinApi.ITradeApi.dll x64\Release\XFinApi.ITradeApi.dll /Y
+
+xcopy ..\XTA_W64\Api\ESITap_v9.3.0.1_20160406 x64\Debug\XTA_W64\Api\ESITap_v9.3.0.1_20160406 /I /E /Y
+copy ..\XTA_W64\Cpp\XFinApi.ITradeApid.dll x64\Debug\XFinApi.ITradeApid.dll /Y
+
+pause
